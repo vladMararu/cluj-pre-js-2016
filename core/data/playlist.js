e643585db431bba24617ff0654d65e9db6472ea9
Septimiu Criste <septimiu.criste@softvision.ro>
2016-03-18 14:56:58 +0200

    Add playlist json

---
+++ core/data/playlist.js
@@ -0,0 +1,146 @@
+{
+  "title": "Playlists",
+  "playlists": [
+    {
+      "id": 0,
+      "songs": [
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "money ea money el",
+          "songAuthor": "Dr. Dre",
+          "songLength": 473
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "enemies enemies ea enemies",
+          "songAuthor": "Eminem",
+          "songLength": 442
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "money enemies ea love",
+          "songAuthor": "Smiley",
+          "songLength": 388
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "love money pierdut money",
+          "songAuthor": "Smiley",
+          "songLength": 224
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "el pierdut el ea",
+          "songAuthor": "Eminem",
+          "songLength": 115
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "enemies pierdut pierdut money",
+          "songAuthor": "Eminem",
+          "songLength": 339
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "ea enemies pierdut enemies",
+          "songAuthor": "Smiley",
+          "songLength": 450
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "money enemies el love",
+          "songAuthor": "Andra",
+          "songLength": 269
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "love pierdut pierdut love",
+          "songAuthor": "Eminem",
+          "songLength": 342
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "money money love ea",
+          "songAuthor": "Andra",
+          "songLength": 146
+        }
+      ]
+    },
+    {
+      "id": 1,
+      "songs": [
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "love enemies el enemies",
+          "songAuthor": "Dr. Dre",
+          "songLength": 30
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "enemies love enemies ea",
+          "songAuthor": "Andra",
+          "songLength": 162
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "money love money pierdut",
+          "songAuthor": "Eminem",
+          "songLength": 363
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "money pierdut ea pierdut",
+          "songAuthor": "Andra",
+          "songLength": 205
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "el ea ea money",
+          "songAuthor": "Eminem",
+          "songLength": 437
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "songs": [
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "enemies love pierdut love",
+          "songAuthor": "Smiley",
+          "songLength": 122
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "ea ea enemies el",
+          "songAuthor": "Eminem",
+          "songLength": 72
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "enemies pierdut el enemies",
+          "songAuthor": "Smiley",
+          "songLength": 42
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "money love ea money",
+          "songAuthor": "Dr. Dre",
+          "songLength": 200
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "money ea el enemies",
+          "songAuthor": "Smiley",
+          "songLength": 328
+        },
+        {
+          "image": "../core/assets/2015-best-music.png",
+          "songTitle": "enemies money el enemies",
+          "songAuthor": "Andra",
+          "songLength": 229
+        }
+      ]
+    }
+  ]
+}
